7517aa319e53967fd71de8d066f9b0d06da25455
Sebastian Wagner <sebastian.wagner@suse.com>
2020-03-26 17:50:21 +0100

    mgr/cephadm: Add missing decorator
    Caused by a conflict between
    
    * #33581 (770816dc9f5b440fd1fc6cc507a06cd8573f6ea1) and
    * #34091 (a78f42e225bbc041324dc3e565deb207feddad26)
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -2787,6 +2787,7 @@ class CephadmOrchestrator(orchestrator.Orchestrator, MgrModule):
 
         return cephadm_config, deps
 
+    @trivial_completion
     def add_nfs(self, spec):
         return self._add_daemon('nfs', spec, self._create_nfs, self._config_nfs)
 
