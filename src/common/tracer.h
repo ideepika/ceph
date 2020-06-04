dacb31e392a79db1cf38c31474bb7640fd938ee1
Abhinav Singh <singhabhinav9051571833@gmail.com>
2020-06-05 00:37:32 +0530

    1. Eliminated the use of function overloading for BucketLists and ListBucket 2. Added safety brackets to prevent any case of segementation fault 3. moved tracer.h from include to common directory 4. changed the path from absolute to relative of jaeger configuration file
    Signed-off-by: Abhinav Singh <singhabhinav9051571833@gmail.com>

--- src/common/tracer.h
+++ src/common/tracer.h
@@ -20,27 +20,60 @@
 
 #define SIGNED_RIGHT_SHIFT_IS 1
 #define ARITHMETIC_RIGHT_SHIFT 1
+#include <arpa/inet.h>
 #include <yaml-cpp/yaml.h>
+
 #include <jaegertracing/Tracer.h>
 
-using namespace opentracing;
+#ifndef WITH_JAEGER
+  #define WITH_JAEGER
+#endif
 
-typedef std::unique_ptr<opentracing::Span> jspan;
+typedef std::unique_ptr<opentracing::Span> Span;
 
-namespace JTracer {
+class Jager_Tracer{
+  public:
+    Jager_Tracer(){}
+    ~Jager_Tracer(){
+      if(this->tracer == NULL)
+	return;
+      if(!this->isTracerClosed)
+        this->tracer->Close();
+        this->isTracerClosed=true;
+    }
 
-  static void setUpTracer(const char* serviceToTrace) {
-    static auto configYAML = YAML::LoadFile("../src/jaegertracing/config.yml");
-    static auto config = jaegertracing::Config::parse(configYAML);
-    static auto tracer = jaegertracing::Tracer::make(
-	serviceToTrace, config, jaegertracing::logging::consoleLogger());
-    opentracing::Tracer::InitGlobal(
-	std::static_pointer_cast<opentracing::Tracer>(tracer));
-  auto parent_span = tracer->StartSpan("parent");
-  assert(parent_span);
+    void init_tracer(const char* tracerName,const char* filePath){
+        auto yaml = YAML::LoadFile(filePath);
+        auto configuration = jaegertracing::Config::parse(yaml);
+        this->isTracerClosed=false;
+        this->tracer = jaegertracing::Tracer::make(
+        tracerName,
+        configuration,
+        jaegertracing::logging::consoleLogger());
+        opentracing::Tracer::InitGlobal(
+        std::static_pointer_cast<opentracing::Tracer>(tracer));
+    }
+    inline void finish_tracer(){
+      if(!this->isTracerClosed){
+	 this->isTracerClosed=true;
+	 this->tracer->Close();
+       }
+    }
+    Span new_span(const char* spanName){
+      Span span=opentracing::Tracer::Global()->StartSpan(spanName);
+      return std::move(span);
+    }
+    Span child_span(const char* spanName,const Span& parentSpan){
+      Span span = opentracing::Tracer::Global()->StartSpan(spanName, {opentracing::ChildOf(&parentSpan->context())});
+      return std::move(span);
+    }
+    Span followup_span(const char *spanName, const Span& parentSpan){
+    Span span = opentracing::Tracer::Global()->StartSpan(spanName, {opentracing::FollowsFrom(&parentSpan->context())});
+    return std::move(span);
+  }
+private:
+  std::shared_ptr<opentracing::v2::Tracer> tracer = NULL;
+  bool isTracerClosed;
+};
 
-  parent_span->Finish();
-  tracer->Close();
-}
-}
 #endif
