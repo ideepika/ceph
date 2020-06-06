bfa6b6cea6dccca06d115e2d7d97ea6170d15ad6
Abhinav Singh <singhabhinav9051571833@gmail.com>
2020-06-06 09:02:59 +0530

    Tracer Added for GetObj
    Signed-off-by: Abhinav Singh <singhabhinav9051571833@gmail.com>

--- src/common/tracer.h
+++ src/common/tracer.h
@@ -38,29 +38,26 @@ class Jager_Tracer{
         this->tracer->Close();
         this->isTracerClosed=true;
     }
-
- void init_tracer(const char* tracerName,const char* filePath){
-     auto yaml = YAML::LoadFile(filePath);
-     auto configuration = jaegertracing::Config::parse(yaml);
-     this->isTracerClosed=false;
-     this->tracer = jaegertracing::Tracer::make(
-     tracerName,
-     configuration,
-     jaegertracing::logging::consoleLogger());
-     opentracing::Tracer::InitGlobal(
-     std::static_pointer_cast<opentracing::Tracer>(tracer));
-     auto parent_span = tracer->StartSpan("parent");
-     assert(parent_span);
-
-     parent_span->Finish();
-     tracer->Close();
- }
+  void init_tracer(const char* tracerName,const char* filePath){
+      auto yaml = YAML::LoadFile(filePath);
+      auto configuration = jaegertracing::Config::parse(yaml);
+      this->isTracerClosed=false;
+      this->tracer = jaegertracing::Tracer::make(
+      tracerName,
+      configuration,
+      jaegertracing::logging::consoleLogger());
+      opentracing::Tracer::InitGlobal(
+      std::static_pointer_cast<opentracing::Tracer>(tracer));
+      tracer->Close();
+  }
  inline void finish_tracer(){
+   if(this->tracer == NULL)
+     return;
    if(!this->isTracerClosed){
       this->isTracerClosed=true;
       this->tracer->Close();
     }
- }
+  }
  Span new_span(const char* spanName){
    Span span=opentracing::Tracer::Global()->StartSpan(spanName);
    return std::move(span);
@@ -72,9 +69,9 @@ class Jager_Tracer{
  Span followup_span(const char *spanName, const Span& parentSpan){
  Span span = opentracing::Tracer::Global()->StartSpan(spanName, {opentracing::FollowsFrom(&parentSpan->context())});
  return std::move(span);
-}
-private:
-  std::shared_ptr<opentracing::v2::Tracer> tracer = NULL;
-  bool isTracerClosed;
+ }
+ private:
+   std::shared_ptr<opentracing::v2::Tracer> tracer = NULL;
+   bool isTracerClosed;
 };
 #endif
