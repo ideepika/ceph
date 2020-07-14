fb3ebdcf955a582f2121ecb6bc5d55f87ad735b8
Deepika Upadhyay <dupadhya@redhat.com>
2020-07-14 20:06:52 +0530

    Jaeger: add copyright headers
    Signed-off-by: Deepika Upadhyay <dupadhya@redhat.com>

--- cmake/modules/BuildJaeger.cmake
+++ cmake/modules/BuildJaeger.cmake
@@ -1,3 +1,14 @@
+###############################################################################
+#Ceph - scalable distributed file system
+#
+#Copyright (C) 2020 Red Hat Inc.
+#
+#This is free software; you can redistribute it and/or
+#modify it under the terms of the GNU Lesser General Public
+#License version 2.1, as published by the Free Software
+#Foundation.  See file COPYING.
+################################################################################
+#
 # This module builds Jaeger after it's dependencies are installed and discovered
 # OpenTracing: is built using cmake/modules/BuildOpenTracing.cmake
 # Thrift: build using cmake/modules/Buildthrift.cmake
